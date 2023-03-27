60885fe366577f297e2af7832cdee80daa6c037f
Marius <marius_matulevicius@epam.com>
2023-03-27 20:31:06 +0300

    sign in and new post

--- Paskaita8-9/frontend/social-media-app/src/App.js
+++ Paskaita8-9/frontend/social-media-app/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import MainLayout from './components/layouts/MainLayout/MainLayout';
 import RegistrationLayout from './components/layouts/RegistrationLayout/RegistrationLayout';
+import SignInLayout from './components/layouts/SignInLayout/SignInLayout';
+import SignOutButton from './components/SignOutButton/SignOutButton';
 
 function App() {
   return (
     <div>
-      <RegistrationLayout />
+      <MainLayout />
+      {/* <SignOutButton />
+      <SignInLayout /> */}
+      {/* <RegistrationLayout /> */}
     </div>
   );
 }
